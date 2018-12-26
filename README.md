f52f18f56d4865a7e9dc8401757cc2110dcc85b2
iHDeveloper <hamzadevelop@gmail.com>
2018-12-26 13:11:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Selfbot for streaming and watching
+A selfbot in discord for streaming and watching
+
+## Getting Started
+- `git clone https://github.com/iHDeveloper/selfbot-streaming`
---
+++ index.js
@@ -0,0 +1,44 @@
+const DISCORD_TOKEN = process.env[2]
+const Discord = require('discord.js')
+const client = new Discord.Client({
+    fetchAllMembers: false,
+    disableEveryone: true
+})
+
+client.on('ready', () => {
+    console.log(`Loggined in as ${client.user.tag}`)
+})
+
+client.on('message', (message) => {
+    if (message.author.id !== client.user.id) return undefined;
+    if (message.content.startsWith("=streaming")) {
+        let command = message.content.replace("=streaming", "").trim()
+        if (command === "") {
+            client.user.setActivity();
+            console.log("Cleaned from streaming")
+        } else {
+            client.user.setActivity(command, {
+                type: 'STREAMING',
+                url: 'https://twitch.tv/iHDeveloper'
+            })
+            console.log("Set the streaming to " + command)
+        }
+        message.delete();
+    } else if (message.content.startsWith('=watching')) {
+        let command = message.content.replace("=watching", "").trim()
+        if (command === "") {
+            client.user.setActivity()
+            console.log("Cleaned from watching")
+        } else {
+            client.user.setActivity(command, {
+                type: 'WATCHING'
+            })
+            console.log("Set the watching to " + command)
+        }
+        message.delete();
+    }
+})
+
+console.log("Logging in...")
+
+client.login(DISCORD_TOKEN)
